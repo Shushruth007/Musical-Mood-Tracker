19340c072d223fa084c2ebbd47bbbd0e0a9423b4
faith0disandro <fdisandr@ucsd.edu>
2022-04-09 12:00:14 -0700

    added calendar

--- mood_app/src/App.js
+++ mood_app/src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import format from "date-fns/format";
+import getDay from "date-fns/getDay";
+import parse from "date-fns/parse";
+import startOfWeek from "date-fns/startOfWeek";
+import React, { useState } from "react";
+import { Calendar, dateFnsLocalizer } from "react-big-calendar";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import "./App.css";
+
+const locales = {
+  "en-US": require("date-fns/locale/en-US"),
+};
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales,
+});
+
+const events = [
+  {
+      title: "Big Meeting",
+      allDay: true,
+      start: new Date(2021, 6, 0),
+      end: new Date(2021, 6, 0),
+  },
+  {
+      title: "Vacation",
+      start: new Date(2021, 6, 7),
+      end: new Date(2021, 6, 10),
+  },
+  {
+      title: "Conference",
+      start: new Date(2021, 6, 20),
+      end: new Date(2021, 6, 23),
+  },
+];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Calendar localizer={localizer} events={events} 
+      startAccessor="start" endAccessor="end" style={{ height: 500, margin: "50px" }} />
     </div>
   );
 }
