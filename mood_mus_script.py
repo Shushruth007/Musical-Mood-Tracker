e607148dbc4ed376e082a9db47813bfabdeb3c4a
KrishivVora <45866881+KrishivVora@users.noreply.github.com>
2022-04-09 07:29:07 -0700

    added function to normalize loudness + other small changes

--- mood_mus_script.py
+++ mood_mus_script.py
@@ -1,5 +1,6 @@
 import spotipy as sp
 from spotipy.oauth2 import SpotifyClientCredentials
+from sklearn import preprocessing
 import os
 import pandas as pd
 import argparse
@@ -7,18 +8,23 @@ import argparse
 os.environ["SPOTIPY_CLIENT_ID"] = "988f2b18b9da4913a53774eae0b618fa"
 os.environ["SPOTIPY_CLIENT_SECRET"] = "d1abb213da9040db907cf8a0573c7dfa"
 
-def get_track_features(track_id,sp):
+def get_track_features(track_id, sp):
     if track_id is None:
         return None
     else:
         features = sp.audio_features([track_id])
     return features
 
-def get_features(tracks,sp, mood):
+def normalize_loudness(loudness):
+    min_max_scaler = preprocessing.MinMaxScaler()
+    loudness_scaled = min_max_scaler.fit_transform(loudness)
+    return loudness_scaled
+
+def get_features(tracks, sp, mood):
     tracks_with_features=[]
 
     for track in tracks:
-        features = get_track_features(track['id'],sp)
+        features = get_track_features(track['id'], sp)
         print (track['name'])
         if not features:
             print("passing track %s" % track['name'])
@@ -59,8 +65,12 @@ def get_tracks_from_playlist(playlist, sp):
 def write_to_csv(track_features, filename):
     df = pd.DataFrame(track_features)
     df.drop_duplicates(subset=['name','artist'])
+
+    # normalizing loudness
+    df['loudness'] = pd.DataFrame(normalize_loudness(df[['loudness']].values))
+
     print ('Total tracks in data set', len(df))
-    df.to_csv(filename ,index=False)
+    df.to_csv(filename, index=False)
 
 
 
@@ -70,7 +80,7 @@ def main(playlist, mood, filename):
     print ("Getting user tracks from playlists")
     tracks = get_tracks_from_playlist(playlist, spot)
     print ("Getting track audio features")
-    tracks_with_features = get_features(tracks,spot, mood)
+    tracks_with_features = get_features(tracks, spot, mood)
     print ("Storing into csv")
     write_to_csv(tracks_with_features, filename)
 
