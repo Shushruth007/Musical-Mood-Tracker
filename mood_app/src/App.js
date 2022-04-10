7931bcc0fd5fe07f2c7c625cb9100c2811c72009
faith0disandro <fdisandr@ucsd.edu>
2022-04-09 18:12:22 -0700

    working on mood colors

--- mood_app/src/App.js
+++ mood_app/src/App.js
@@ -4,14 +4,60 @@ import 'react-calendar/dist/Calendar.css';
 
 import './App.css';
 
-function App() {
-  const [value, onChange] = useState(new Date());
+
+
+var dict = {
+  Great: rgb(1, 151, 18),
+  Good: rgb(174, 255, 0),
+  Okay: rgb(251, 255, 0),
+  Sad: rgb(255, 170, 0),
+  VerySad: rgb(255, 0, 0)
+};
+
+
+
+class App extends React.Component {
+  constructor(props)
+  {
+    super(props)
+    this.state={
+      date:new Date(),
+      dayColor: "red"
+    }
+  }
+
+  previouslySelectedDates = []
+
+   tileClass = ({date, view}) => {
+    if (view == 'month') {
+      if (previouslySelectedDates.find(currentDate => currentDate == date)) {
+        return 'newClass'
+      }
+    }
+   }
+
+  onChange = date => {
+    this.previouslySelectedDates.add(date)
+    this.setState({ date })
+  }
+  render()
+  {
 
   return (
-    <div>
-      <Calendar onChange={onChange} value={value}/>
+
+    <div className="App">
+
+       <Calendar
+          onChange={this.onChange}
+          value={this.state.date}
+          tileClassName={tileClass}
+
+        />
     </div>
+
+
   );
+  }
 }
 
 export default App;
