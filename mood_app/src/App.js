9697d00235ba30bddd3df38334a85c416ac79a19
faith0disandro <fdisandr@ucsd.edu>
2022-04-09 22:59:47 -0700

    curent day changes color based on mood

--- mood_app/src/App.js
+++ mood_app/src/App.js
@@ -16,31 +16,41 @@ function isWithinRange(date, range) {
   return isWithinInterval(date, { start: range[0], end: range[1] });
 }
 
-function isWithinRanges(date, ranges) {
-  return ranges.some(range => isWithinRange(date, range));
-}
-
 
 
 var moods = {
-  'Great': '#FF0000',
-  'Good': 'FF8F00',
-  'Okay': 'FBFF00',
-  'Sad': 'AEFF00',
-  'VerySad': '03D015'
+  'Verysad': '#FF0000',
+  'Sad': '#FF8F00',
+  'Okay': '#FBFF00',
+  'Good': '#AEFF00',
+  'Great': '#03D015'
 };
 
-const mood = 'Sad'
+const mood = 'Great' //Change to test the different moods
 const today = new Date();
 
-const datesToAddClassTo = [];
+const datesToAddClassTo = [today];
 
 function tileClassName({ date, view }) {
   // Add class to tiles in month view only
   if (view === 'month') {
     // Check if a date React-Calendar wants to check is on the list of dates to add class to
     if (datesToAddClassTo.find(dDate => isSameDay(dDate, date))) {
-      return 'myClassName';
+      if (mood == 'Verysad') {
+        return 'verysad';
+      }
+      else if (mood == 'Sad') {
+        return 'sad';
+      }
+      else if (mood == 'Okay') {
+        return 'okay';
+      }
+      else if (mood == 'Good') {
+        return 'good';
+      }
+      else {
+        return 'great';
+      }
     }
   }
 }
@@ -56,8 +66,6 @@ function App() {
       <h1 className='text-center'>Spotify Mood Calender</h1>
       <div className='calendar-container'>
         <Calendar 
-        onChange={setDate}
-        defaultValue={date} 
         tileClassName={tileClassName}/>
       </div>
       <p className='text-center'>
